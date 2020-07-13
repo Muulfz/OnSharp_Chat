fb7a09e941e944b2a4027259c8f675ba82bb06e3
Sheiyla <sheiylanie@ovh.fr>
2020-07-13 16:15:58 +0200

    Initial Plugin

---
+++ OnSharp_Chat.cs
@@ -0,0 +1,28 @@
+﻿using Onsharp.Entities;
+using Onsharp.Events;
+using Onsharp.Plugins;
+using System;
+
+namespace OnSharp_Chat
+{
+
+    [PluginMeta("onsharp-chat", "Chat Plugin", "1.0", "Codeskull", IsDebug = true)]
+    public class OnSharp_Chat : Plugin
+    {
+
+        public override void OnStart() { }
+        public override void OnStop() { }
+
+        [ServerEvent(EventType.PlayerChat)]
+        public void OnChat(Player player, string text)
+        {
+            var hourMinutesec = DateTime.Now.ToString("HH:mm:ss");
+            foreach (var pl in Server.Players)
+            {
+                pl.SendMessage("<span color=\"#2397e7\" size=\"10\">["+ hourMinutesec + "]</> <span color=\"#e7a523\" size=\"10\">" + pl.Name + " :</> <span color=\"#ffffff\" size=\"10\">" + text + "</>");
+            } 
+        }
+
+    }
+
+}
